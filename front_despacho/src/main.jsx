386e51e299f1ba571bad732e2343fec76fd91270
Javier-Munozq <javie.munozq@duocuc.cl>
2026-06-23 19:39:21 -0400

    Cambio en el frontend

--- front_despacho/src/main.jsx
+++ front_despacho/src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <h1 style={{ textAlign: 'center', backgroundColor: '#4CAF50', color: 'white', padding: '10px' }}>
+      Prueba de que el ci/cd esta funcionando correctamente
+    </h1>
     <AppRoutes />
   </React.StrictMode>
 );
